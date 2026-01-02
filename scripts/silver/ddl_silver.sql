dc5e5de85cf3b7fd5310d80a8a59e2e2adbe47f8
sai srujan <vsaisrujan@users.noreply.github.com>
2026-01-01 19:18:59 -0500

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,1 +1,80 @@
 
+create table silver.crm_cust_info (
+	cst_id int,cst_key Nvarchar(50),
+	cst_firstname Nvarchar(50),
+	cst_lastname Nvarchar(50),
+	cst_marital_status Nvarchar(50),
+	cst_gndr Nvarchar(50),
+	cst_create_date DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+-- =============================================
+-- Table: silver.crm_prd_info
+-- Purpose: Store raw product data from CRM source
+-- Source File: prd_info.csv
+-- Load Type: Full / Incremental (raw ingestion)
+-- =============================================
+IF OBJECT_ID ('silver.crm_prd_info','U') IS NOT NULL
+	DROP TABLE silver.crm_prd_info;
+CREATE TABLE silver.crm_prd_info (
+    prd_id         INT,
+	cat_id		   NVARCHAR(50), 
+    prd_key        NVARCHAR(50),        
+    prd_nm         NVARCHAR(100),       
+    prd_cost       INT,					 
+    prd_line       NVARCHAR(50),        
+    prd_start_dt   DATE,              
+    prd_end_dt     DATE,			
+	dwh_create_date DATETIME2 DEFAULT GETDATE()                 
+);
+GO
+
+-- =============================================
+-- Table: silver.crm_sales_details
+-- Purpose: Store raw sales transaction data
+-- Source File: sales_details.csv
+-- Load Type: Transactional fact data (raw)
+-- =============================================
+IF OBJECT_ID ('silver.crm_sales_details','U') IS NOT NULL
+	DROP TABLE silver.crm_sales_details;
+CREATE TABLE silver.crm_sales_details (
+    sls_ord_num    NVARCHAR(50),        
+    sls_prd_key    NVARCHAR(50),        
+    sls_cust_id    INT,                 
+    sls_order_dt   DATE,                
+    sls_ship_dt    DATE,                
+    sls_due_dt     DATE,                
+    sls_sales      INT,      
+    sls_quantity   INT,                
+    sls_price      INT,       
+	dwh_create_date DATETIME2 DEFAULT GETDATE()  
+);
+GO
+
+create table silver.erp_loc_a101 (
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()  
+);
+GO
+
+create table silver.erp_cust_az12 (
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()  
+);
+GO
+
+create table silver.erp_px_cat_g1v2 (
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()  
+);
+
+
+
+

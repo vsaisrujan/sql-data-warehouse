f7f5bbdf210da7a2dcb73e9fe1370606fb58c12a
sai srujan <vsaisrujan@users.noreply.github.com>
2026-01-02 22:38:41 -0500

    Update quality_checks.sql

--- tests/quality_checks.sql
+++ tests/quality_checks.sql
@@ -105,3 +105,79 @@ select * from bronze.erp_px_cat_g1v2 where cat!=TRIM(cat) or subcat!=TRIM(subcat
 Select DISTINCT cat from bronze.erp_px_cat_g1v2
 
 select * from silver.erp_px_cat_g1v2
+
+-----------------------------------------------------------------------------------------------------------
+-- Quality Chekc for Gold Layer after joining three tables ,to identify duplicates after joining 3 tables
+--gold.dim_customers 
+-----------------------------------------------------------------------------------------------------------
+
+WITH cust_joined AS (
+    SELECT 
+        ci.cst_id,
+        ci.cst_key,
+        ci.cst_firstname,
+        ci.cst_lastname,
+        ci.cst_marital_status,
+        ci.cst_gndr,
+        ci.cst_create_date,
+        ca.bdate,
+        ca.gen,
+        la.cntry
+    FROM silver.crm_cust_info ci 
+    LEFT JOIN silver.erp_cust_az12 ca 
+        ON ci.cst_key = ca.cid
+    LEFT JOIN silver.erp_loc_a101 la 
+        ON ci.cst_key = la.cid
+)
+SELECT
+    cst_id,
+    COUNT(*) AS record_count
+FROM cust_joined
+GROUP BY cst_id
+HAVING COUNT(*) > 1;
+
+select distinct gender from gold.dim_customers 
+
+-----------------------------------------------------------------------------------------------------------
+--gold.dim_products
+-----------------------------------------------------------------------------------------------------------
+
+with prd_joined as (
+SELECT 
+	pn.prd_id,
+	pn.cat_id,
+	pn.prd_key,
+	pn.prd_nm,
+	pn.prd_cost,
+	pn.prd_line,
+	pn.prd_start_dt,
+	pc.cat,
+	pc.subcat,
+	pc.maintenance
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pn.cat_id=pc.id
+WHERE prd_end_dt IS NULL
+)
+
+select prd_key,COUNT(*) from prd_joined group by prd_key having COUNT(*)>1
+
+select * from gold.dim_products
+
+
+-----------------------------------------------------------------------------------------------------------
+--gold.fact_sales
+-----------------------------------------------------------------------------------------------------------
+
+select * from gold.fact_sales
+
+--Fact Check - check if all dimension tables can successfully join to the fact table
+select * from gold.fact_sales f
+LEFT JOIN gold.dim_customers c 
+ON c.customer_key=f.customer_key
+WHERE c.customer_key IS NULL
+
+select * from gold.fact_sales f
+LEFT JOIN gold.dim_products p 
+ON p.product_key=f.product_key
+WHERE p.product_key IS NULL
